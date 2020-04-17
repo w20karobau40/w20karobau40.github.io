e5b8e5f01c2dc1250109bb409a62fe397883284e
Tobias Heinrich <Tobias.Heinrich@iwu.fraunhofer.de>
2020-04-17 15:33:06 +0200

    small fix

--- wordpress/create_wordpress_js.ps1
+++ wordpress/create_wordpress_js.ps1
@@ -1,6 +1,6 @@
 $filename = ".\wordpress.js"
 # disable IDE formatter
-Add-Content -Path $filename -Value "// @formatter:off"
+Set-Content -Path $filename -Value "// @formatter:off"
 # Fetch d3.js
 Invoke-RestMethod -Uri "https://d3js.org/d3.v5.min.js" -Method Get | Out-File $filename -Append
 Invoke-RestMethod -Uri "https://d3js.org/d3-array.v2.min.js" -Method Get | Out-File $filename -Append
@@ -10,4 +10,4 @@ $main_js = Get-Content -Path "..\visualization\main.js" -Raw -Encoding UTF8
 $postParams = @{ input = $main_js }
 Invoke-RestMethod -Uri "https://javascript-minifier.com/raw" -Method Post -Body $postParams | Out-File $filename -Append
 # reenable IDE formatter
-Add-Content -Path $filename -Value "// @formatter:off"
+Add-Content -Path $filename -Value "// @formatter:on"
