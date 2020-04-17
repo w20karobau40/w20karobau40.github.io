50e8ac5159439ebd1b9d6efc93de152c49f92282
Tobias Heinrich <Tobias.Heinrich@iwu.fraunhofer.de>
2020-04-17 14:37:56 +0200

    update wordpress.js

--- wordpress/create_wordpress_js.ps1
+++ wordpress/create_wordpress_js.ps1
@@ -1,8 +1,13 @@
+$filename = ".\wordpress.js"
+# disable IDE formatter
+Add-Content -Path $filename -Value "// @formatter:off"
 # Fetch d3.js
-Invoke-RestMethod -Uri "https://d3js.org/d3.v5.min.js" -Method Get | Out-File .\wordpress.js
-Invoke-RestMethod -Uri "https://d3js.org/d3-array.v2.min.js" -Method Get | Out-File .\wordpress.js -Append
+Invoke-RestMethod -Uri "https://d3js.org/d3.v5.min.js" -Method Get | Out-File $filename -Append
+Invoke-RestMethod -Uri "https://d3js.org/d3-array.v2.min.js" -Method Get | Out-File $filename -Append
 # Minify main.js
-Add-Content -Path .\wordpress.js -Value "//main.js"
+Add-Content -Path $filename -Value "// main.js"
 $main_js = Get-Content -Path "..\visualization\main.js" -Raw -Encoding UTF8
 $postParams = @{ input = $main_js }
-Invoke-RestMethod -Uri "https://javascript-minifier.com/raw" -Method Post -Body $postParams | Out-File .\wordpress.js -Append
+Invoke-RestMethod -Uri "https://javascript-minifier.com/raw" -Method Post -Body $postParams | Out-File $filename -Append
+# reenable IDE formatter
+Add-Content -Path $filename -Value "// @formatter:off"
