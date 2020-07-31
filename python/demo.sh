0d65934dc9aa7314734c08868c27ff940964c15f
Tobias Heinrich <Tobias.Heinrich@iwu.fraunhofer.de>
2020-07-31 02:04:46 +0200

    another attempt

---
+++ python/demo.sh
@@ -0,0 +1,1 @@
+ curl -X POST -d '{"method": "export_responses", "params": ["cNUaugbYWywtX7YlvPzLocRqJAqzPTeO", 197925, "json"], "id": 2}' -H "Content-Type: application/json" https://websites.fraunhofer.de/iwu-surveys/index.php/admin/remotecontrol
