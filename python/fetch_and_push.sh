26a3b762e18e11098c6749e8fcdbc0f8424aa82a
Tobias Heinrich <Tobias.Heinrich@iwu.fraunhofer.de>
2020-08-05 11:40:14 +0200

    actually commit file

--- python/fetch_and_push.sh
+++ python/fetch_and_push.sh
@@ -3,4 +3,5 @@ git pull
 python get_limesurvey_results.py
 cp limesurvey_data.js ../assets/js/visualization/limesurvey_data.js
 git add ../assets/js/visualization/limesurvey_data.js
+git commit -m "automatic limesurvey update"
 git push
